99d3e84bbf58cafa1962db3c8e865899802222cb
Jonathan  Guimarães <92338228+JonathanGuimarae3s@users.noreply.github.com>
2022-09-05 08:29:46 -0300

    Update exe28.js

--- logica de programação/js/exercicios/exe28.js
+++ logica de programação/js/exercicios/exe28.js
@@ -5,9 +5,7 @@ let lado2 = parseInt(prompt("escreva o lado2?"));
 if (lado1 == lado2 || lado2 == base || base == lado1) {
 	console.log("Insira numeros diferente");
 } else if (
-	lado1 + lado2 > base &&
-	base + lado2 > lado1 &&
-	lado2 + base > lado1
+	
 ) {
 	console.log("é um triangulo");
 }
