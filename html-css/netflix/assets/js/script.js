2f71de8dc7a235b4318bc660d5c3f7f56bc61610
joanthan <guimaraesjonathan92@gmail.com>
2022-10-24 11:48:42 -0300

    update

--- html-css/netflix/assets/js/script.js
+++ html-css/netflix/assets/js/script.js
@@ -4,14 +4,15 @@ onload = () => {
 
 	while (filmesId!=9999999) {
        let idURL = toString(filmesId)
-		let url = `http://www.omdbapi.com/?i=tt${idURL}&apikey=9ae51d8c`;
-		fetch(url)
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				console.log(data);
-			});
+	   console.log(idURL)
+		// let url = `http://www.omdbapi.com/?i=tt${idURL}&apikey=9ae51d8c`;
+		// fetch(url)
+		// 	.then((response) => {
+		// 		return response.json();
+		// 	})
+		// 	.then((data) => {
+		// 		console.log(data);
+		// 	});
 		filmesId--;
 	}
 };
