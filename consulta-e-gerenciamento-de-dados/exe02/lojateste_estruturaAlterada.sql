3caff66801187d868a824c3fb9afef12562f133d
Jonathan  Guimarães <92338228+JonathanGuimarae3s@users.noreply.github.com>
2022-11-01 08:21:05 -0300

    updates

--- consulta-e-gerenciamento-de-dados/exe02/lojateste_estruturaAlterada.sql
+++ consulta-e-gerenciamento-de-dados/exe02/lojateste_estruturaAlterada.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 31-Out-2022 às 15:30
+-- Generation Time: 31-Out-2022 às 15:53
 -- Versão do servidor: 10.1.39-MariaDB
 -- versão do PHP: 7.3.5
 
