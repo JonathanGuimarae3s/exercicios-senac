30db511b1751ff9c6d602e8b037274e6121bf68c
Jonathan  Guimarães <92338228+JonathanGuimarae3s@users.noreply.github.com>
2022-11-11 10:30:45 -0300

    consumindo a api tmdb

--- html-css/netflix/assets/js/script.js
+++ html-css/netflix/assets/js/script.js
@@ -1,12 +1,12 @@
 onload = () => {
 	let key = "c81196be7f4ffd3f4c9c37554690999f";
-	let url = `https://api.themoviedb.org/3/movie/76341?api_key=${key}&language=pt-BR`;
+	let url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=pt-BR&page=1`;
 	fetch(url)
-		.then(function (resp) {
-			resp.json()
-		}).then(function (data))=>{
-			console.log(data)
-			
+		.then((resp) => {
+			return resp.json();
+		})
+		.then((data) => {
+			console.log(data);
 		})
 		.catch(function (error) {
 			console.log(
